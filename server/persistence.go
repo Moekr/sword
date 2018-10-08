290693c256e756cf77f210db273926b718d264cc
Moekr <highter@outlook.com>
2018-10-08 16:58:08 +0800

    备份文件由zip+store改为tar+gzip

--- server/persistence.go
+++ server/persistence.go
@@ -1,7 +1,8 @@
 package server
 
 import (
-	"archive/zip"
+	"archive/tar"
+	"compress/gzip"
 	"encoding/json"
 	"fmt"
 	"github.com/Moekr/sword/util"
@@ -62,14 +63,17 @@ func saveData() {
 
 func backupData(cur time.Time) {
 	d := args.DataDir
-	f := fmt.Sprintf("backup-%d.zip", cur.Unix())
+	f := fmt.Sprintf("backup-%d.tar.gz", cur.Unix())
 	zipFile, err := os.Create(path.Join(d, f))
 	if err != nil {
 		util.Infof("create backup file %s error: %s\n", f, err.Error())
 		return
 	}
-	zw := zip.NewWriter(zipFile)
-	defer zw.Close()
+	defer zipFile.Close()
+	gw := gzip.NewWriter(zipFile)
+	defer gw.Close()
+	tw := tar.NewWriter(gw)
+	defer tw.Close()
 	filepath.Walk(d, func(path string, info os.FileInfo, err error) error {
 		if info.IsDir() && path != d {
 			return filepath.SkipDir
@@ -83,14 +87,14 @@ func backupData(cur time.Time) {
 			util.Debugf("open file %s error: %s\n", path, err.Error())
 			return nil
 		}
-		if header, err := zip.FileInfoHeader(info); err != nil {
-			util.Debugf("create zip header %s error%s\n", path, err.Error())
+		if header, err := tar.FileInfoHeader(info, ""); err != nil {
+			util.Debugf("create tar header %s error%s\n", path, err.Error())
 		} else {
 			header.Name = filepath.Base(header.Name)
-			if w, err := zw.CreateHeader(header); err != nil {
-				util.Debugf("add zip header %s error%s\n", path, err.Error())
-			} else if _, err := io.Copy(w, file); err != nil {
-				util.Debugf("write zip file %s error\n", path, err.Error())
+			if err := tw.WriteHeader(header); err != nil {
+				util.Debugf("add tar header %s error%s\n", path, err.Error())
+			} else if _, err := io.Copy(tw, file); err != nil {
+				util.Debugf("write tar file %s error\n", path, err.Error())
 			}
 		}
 		return nil
